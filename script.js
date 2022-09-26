58cd5dbbfbf89cb91448d666c6bff79f9e18fc56
dahunsi halima <dahunsiolajumoke18@gmail.com>
2022-09-26 06:12:43 -0700

    all filed tracked

---
+++ script.js
@@ -0,0 +1,51 @@
+let MyName= "Dahunsi"
+MyName += " Halima"
+MyName += " Olajumoke"
+console.log (MyName)
+let CarNumber = "Vol23"
+CarNumber += 57
+CarNumber += "Abj" 
+console.log (CarNumber)
+let MonthlyIncome= 500000
+MonthlyIncome /= 2
+MonthlyIncome /= 1
+console.log (MonthlyIncome)
+let SleepingHours= 135
+SleepingHours %= 50
+SleepingHours %= 7
+console.log (SleepingHours)
+
+let MyAge = 20
+MyAge -= 2
+MyAge -= 10
+console.log (MyAge)
+let orange = 50
+orange *= 2
+orange *= 1
+console.log (orange)
+
+document.getElementById("head").innerHTML="My Javascript day 3 experience";
+    document.getElementById("head1").innerHTML="I had my Javascript day3 class on the 20th of september, 2022. the class was fun and interesting."
+    document.getElementById("head2").innerHTML="Below are the lists of what i learnt in the class."
+    document.getElementById("discussion1").innerHTML="I learnt about the javascript's operators and the different kinds of operators in javascript."
+    document.getElementById("discussion2").innerHTML="I also learnt about javascript's datatypes which include; String, numbers, booleans, null, arrays, objects and some other datatypes"
+    document.getElementById("capt").innerHTML="Assignment"
+    document.getElementById("capt2").innerHTML="Assignment Operators"
+    document.getElementById("addition").innerHTML=MyName
+    document.getElementById("addition2").innerHTML=CarNumber
+    document.getElementById("divide").innerHTML=MonthlyIncome
+    document.getElementById("module").innerHTML=SleepingHours
+    document.getElementById("multiply").innerHTML=orange
+    document.getElementById("substract").innerHTML=MyAge
+    document.getElementById("operators").innerHTML="ADDITION ASSIGNMENT OPERATOR"
+    document.getElementById("operator1").innerHTML=" <code>let MyName= \"Dahunsi\"</code> <br><code>MyName += \"Halima\"</code> <br> <code>MyName += \"Olajumoke\"</code> <br>"
+    document.getElementById("operator1a").innerHTML=" <code>let CarNumber= \"Vol23\"</code> <br><code>CarNumber += 57</code> <br> <code>CarNumber += \"Abj\"</code> <br>"
+    document.getElementById("operator2").innerHTML="SUBSTRACTION ASSIGNMENT OPERATOR"
+    document.getElementById("substraction").innerHTML=" <code>let MyAge= 20</code> <br><code>MyAge -= 2</code> <br> <code>MyAge -= 10</code> <br>"
+    document.getElementById("operator3").innerHTML="DIVISION ASSIGNMENT OPERATOR"
+    document.getElementById("division").innerHTML=" <code>let MonthlyIncome= #500,000</code> <br><code>MonthlyIncome /= 2</code> <br> <code>MonthlyIncome /= 1</code> <br>"
+    document.getElementById("operator4").innerHTML="MODULUS ASSIGNMENT OPERATOR"
+    document.getElementById("modulus").innerHTML=" <code>let SleepingHours= 135</code> <br><code>SleepingHours %= 50</code> <br> <code>SleepingHours %= 7</code> <br>"
+    document.getElementById("operator5").innerHTML="MULTIPLICATION ASSIGNMENT OPERATOR"
+    document.getElementById("multiplication").innerHTML=" <code>let orange = 50</code> <br><code>orange =* 2</code> <br> <code>orange *= 1</code> <br>"
+
